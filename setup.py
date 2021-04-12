140fd5e5793a7c4d4e560fa1ebb4877e552bef5e
Tommer-R <76598250+Tommer-R@users.noreply.github.com>
2021-04-12 15:20:35 +0200

    add setup.py
    Signed-off-by: Tommer-R <76598250+Tommer-R@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TravelingSalesman',
+    version='v1.0',
+    packages=[''],
+    url='https://github.com/Tommer-R/Traveling-salesman',
+    license='MIT',
+    author='Tommer Rissin',
+    author_email='tommerrissin@gmail.com',
+    description='Traveling salesman problem for testing optimization algorithms'
+)
