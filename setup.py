f34da560b19ca450c1b25934a407ca15bf4be946
Tommer-R <76598250+Tommer-R@users.noreply.github.com>
2021-04-13 05:24:19 +0200

    add sub folder
    Signed-off-by: Tommer-R <76598250+Tommer-R@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='TravelingSalesman',
-    version='v1.0',
-    packages=[''],
+    version='v1.0.0',
+    packages=['TravelingSalesman'],
     url='https://github.com/Tommer-R/Traveling-salesman',
     license='MIT',
     author='Tommer Rissin',
     author_email='tommerrissin@gmail.com',
-    description='Traveling salesman problem for testing optimization algorithms'
+    description='Traveling salesman problem'
 )
